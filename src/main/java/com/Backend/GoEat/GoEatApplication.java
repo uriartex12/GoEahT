549004bd4be95680e80c5102ab2ef1d714ec9980
jhon uriarte <prietomatex_1998@hotmail.com>
2023-04-23 12:35:45 -0500

    first

---
+++ src/main/java/com/Backend/GoEat/GoEatApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.GoEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Backend/GoEat/GoEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.GoEat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
